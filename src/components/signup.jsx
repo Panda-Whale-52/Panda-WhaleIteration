0bf1348291bdb979e726bf03b093f0bf4f3e4dd3
DonnaIsabel97 <tpr.th.bvb@gmail.com>
2025-01-14 20:19:06 -0500

    update front end

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -2,17 +2,46 @@
 import { Link } from 'react-router';
 import React from 'react';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import {  useState } from "react";
 
 const Signup = () => {
-  const handleOAuthSignup = (e) => {
-    console.log(`Sign up with OAuthSignup `);
-    // Add OAuth signup logic here
-  };
+  const navigate = useNavigate();
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [nameValue, setNameValue] = useState('');
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log('User signed up');
-    // Add form submission logic here
+  // const handleOAuthSignup = (e) => {
+  //   console.log(`Sign up with OAuthSignup `);
+  //   // Add OAuth signup logic here
+  // };
+
+  const handleFormSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await fetch('http://localhost:3000/api/user/register', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+          name: nameValue,
+          email: emailValue,
+          password: passwordValue,
+        }),
+      });
+      
+      const data = await response.json();
+      
+      if (!response.ok) {
+        throw new Error('Failed to create user');
+      }
+      navigate('/login')
+    } catch (error) {
+      console.error('Signup failed:', error);
+    }
+    
   };
 
   return (
@@ -27,14 +56,14 @@ const Signup = () => {
             <Button
               variant='secondary'
               className='w-100 mb-2'
-              onClick={() => handleOAuthSignup('Github')}
+              // onClick={() => handleOAuthSignup('Github')}
             >
               Sign up with Github
             </Button>
             <Button
               variant='danger'
               className='w-100 mb-2'
-              onClick={() => handleOAuthSignup('Google')}
+              // onClick={() => handleOAuthSignup('Google')}
             >
               Sign up with Google
             </Button>
@@ -45,21 +74,32 @@ const Signup = () => {
           <Form onSubmit={handleFormSubmit} className='mt-3'>
             <Form.Group className='mb-3' controlId='formUsername'>
               <Form.Label>Username</Form.Label>
-              <Form.Control type='text' placeholder='Enter username' required />
+              <Form.Control 
+              type='text' 
+              value={nameValue}  
+              placeholder='Enter username' 
+              onChange={(e) => setNameValue(e.target.value)} 
+              required />
             </Form.Group>
             <Form.Group className='mb-3' controlId='formEmail'>
               <Form.Label>Email</Form.Label>
-              <Form.Control type='email' placeholder='Enter email' required />
+              <Form.Control 
+              type='email' 
+              value={emailValue} 
+              placeholder='Enter email' 
+              onChange={(e) => setEmailValue(e.target.value)}
+              required  />
             </Form.Group>
             <Form.Group className='mb-3' controlId='formPassword'>
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type='password'
                 placeholder='Enter password'
+                onChange={(e) => setPasswordValue(e.target.value)}
                 required
               />
             </Form.Group>
-            <Button variant='success' type='submit' className='w-100'>
+            <Button variant='success' type='submit' className='w-100' >
               Sign up
             </Button>
           </Form>
