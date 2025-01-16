7ca81b9d4989ba3ef771b666571487a3afc7302f
Fabiano Santin <fabiano.santin@gmail.com>
2025-01-15 19:43:14 -0500

    done again

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
 const config = {
-<<<<<<< HEAD
   baseURL: 'http://localhost:3000/api', // Replace with your actual base URL
-=======
-  baseURL: "http://localhost:3000/api", // Replace with your actual base URL
->>>>>>> dev
 };
 
 export default config;
